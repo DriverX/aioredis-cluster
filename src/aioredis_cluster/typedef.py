e8902b4e431dea449518d9846d6a005cc451b0e5
Vadim Pushtaev <pushtaev.vm@gmail.com>
2021-11-11 15:33:31 +0300

    private pool size support
    + number of pools limit via Cluster options

--- src/aioredis_cluster/typedef.py
+++ src/aioredis_cluster/typedef.py
@@ -1,4 +1,4 @@
-from typing import Awaitable, Callable, List, Tuple, Union
+from typing import Awaitable, Callable, List, Tuple, Union, Protocol
 
 from aioredis.commands import Redis
 
@@ -9,5 +9,14 @@ AioredisAddress = Union[str, Tuple[str, int]]
 SlotsResponse = List[List]
 CommandsFactory = Callable[[AbcConnection], Redis]
 PoolerBatchCallback = Callable[[AbcPool], Awaitable[None]]
-PoolCreator = Callable[[AioredisAddress], Awaitable[AbcPool]]
 PubsubResponse = Tuple[bytes, bytes, int]
+
+
+class PoolCreator(Protocol):
+    def __call__(
+        self,
+        addr: AioredisAddress,
+        pool_minsize: int = None,
+        pool_maxsize: int = None,
+    ) -> Awaitable[AbcPool]:
+        ...
