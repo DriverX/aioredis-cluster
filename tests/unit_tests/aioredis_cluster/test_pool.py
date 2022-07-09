0fcb0cb0aa0e55d701e9b585dab013803bd881bd
Anton Ilyushenkov <274471+DriverX@users.noreply.github.com>
2022-07-09 19:24:05 +0300

    +tests

--- tests/unit_tests/aioredis_cluster/test_pool.py
+++ tests/unit_tests/aioredis_cluster/test_pool.py
@@ -88,3 +88,68 @@ async def test_acquire__correct_return_order(mocker):
     assert acquire_task3.result() is conn1
 
     mocked_create_new_connection.assert_awaited_once()
+
+
+async def test_acquire__release_multiple_connections_at_time(mocker):
+    addr = ("127.0.0.1", 6379)
+
+    conn_mocks = [
+        create_conn_mock(),
+        create_conn_mock(),
+        create_conn_mock(),
+    ]
+
+    pool = ConnectionsPool(addr, minsize=1, maxsize=3)
+    mocked_create_new_connection = mocker.patch.object(
+        pool,
+        "_create_new_connection",
+        new=mock.AsyncMock(
+            side_effect=conn_mocks,
+        ),
+    )
+
+    acquired_conn1 = await pool.acquire()
+    acquired_conn2 = await pool.acquire()
+    acquired_conn3 = await pool.acquire()
+
+    conn_waiters = [
+        asyncio.ensure_future(pool.acquire()),
+        asyncio.ensure_future(pool.acquire()),
+        asyncio.ensure_future(pool.acquire()),
+        asyncio.ensure_future(pool.acquire()),
+        asyncio.ensure_future(pool.acquire()),
+        asyncio.ensure_future(pool.acquire()),
+    ]
+
+    await asyncio.sleep(0)
+    await asyncio.sleep(0)
+    await asyncio.sleep(0)
+
+    conn_waiters[4].cancel()
+    pool.release(acquired_conn1)
+    pool.release(acquired_conn3)
+
+    await asyncio.sleep(0)
+    await asyncio.sleep(0)
+    await asyncio.sleep(0)
+
+    # 3 because one is manualy cancelled before
+    assert sum(w.done() for w in conn_waiters) == 3
+    assert conn_waiters[0].result() is acquired_conn1
+    assert conn_waiters[1].result() is acquired_conn3
+
+    pool.release(acquired_conn1)
+    pool.release(acquired_conn2)
+    pool.release(acquired_conn3)
+
+    await asyncio.sleep(0)
+    await asyncio.sleep(0)
+    await asyncio.sleep(0)
+
+    assert sum(w.done() for w in conn_waiters) == 6
+
+    assert conn_waiters[2].result() is acquired_conn1
+    assert conn_waiters[3].result() is acquired_conn2
+    assert conn_waiters[5].result() is acquired_conn3
+
+    mocked_create_new_connection.await_count == 3
